3543961d7829c478b6e4e4719fd6eebffed05e17
IgmarLozadaBolivar <losadabolivar@gmail.com>
2025-04-22 06:53:24 -0500

    Limpieza de repositorio completo y reiniciar estructura

--- apps/web/src/lib/supabase.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-// Usa las claves de configuración de la API del proyecto en Supabase
-const supabaseUrl = "https://puesgjbxmhfjnkeuwrnp.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1ZXNnamJ4bWhmam5rZXV3cm5wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUxNjMyMzUsImV4cCI6MjA2MDczOTIzNX0.5Dw4DWBJ0FvYWLHGLbJ-zEoc6iSuauaA8kPQ5NmasBQ";
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
